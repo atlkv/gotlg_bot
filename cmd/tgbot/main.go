346e53f2468a070f9876abe658381e92028ff27e
AlexeyT <atolokolnikov@ozon.ru>
2022-01-23 17:16:54 +0300

    Return config controller

--- cmd/tgbot/main.go
+++ cmd/tgbot/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (	
-	//"tgbot/internal/config"
+	"tgbot/internal/config"
 	"tgbot/internal/bot"
 	"log"
 )
 
 func main() {
-	// config, err := config.NewConfig("local")
+	config, err := config.NewConfig("local")
 	
-	// if err != nil {
-	// 	log.Panicf("Config error: %s", err)
-	// }
+	if err != nil {
+		log.Panicf("Config error: %s", err)
+	}
 
-	// token, err := config.GetTelegramToken()
+	token, err := config.GetTelegramToken()
 
-	// if err != nil {
-	// 	log.Panicf("Can't get telegram token: %s", err)
-	// }
+	if err != nil {
+		log.Panicf("Can't get telegram token: %s", err)
+	}
 
-	 bot, err := bot.NewBot("5047366335:AAFOoTRzS6CZmDbJ65_PIO5NTe_SKw9tFyI")
+	 bot, err := bot.NewBot(token)
 
 	log.Printf("Success auth by: %s", bot.UserName())
 
